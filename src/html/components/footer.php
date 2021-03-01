10c8453071b7b42359bf3511e2c309a990d17855
Telmooo <telmoabaptista@hotmail.com>
2021-03-01 09:31:29 +0000

    Extracted common components from pages

---
+++ src/html/components/footer.php
@@ -0,0 +1,20 @@
+<?php function site_footer() { ?>
+    <footer class="container-fluid mt-auto bg-dark py-2">
+        <div class="row">
+            <div class="ms-2 col">
+                <div class="row align-items-center justify-content-start">
+                    <img class="col-auto" src="../../static/logo.svg" height="60">
+                    <h4 class="col-6 text-light">Trade-a-Bid</h4>
+                </div>
+                <span class="row text-muted">&copy; Copyright 2021 Trade-a-Bid. All rights reserved</span>
+            </div>
+
+            <div class="col d-flex flex-column text-light align-items-end m-1">
+                <a>About</a>
+                <a>Contacts</a>
+                <a>FAQ</a>
+                <a>Statistics</a>
+            </div>
+        </div>
+    </footer>
+<?php } ?>
