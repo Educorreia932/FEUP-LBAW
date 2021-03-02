174f87e5729150d65ab007ba352a7ebf16d39ce1
Educorreia932 <skelozard1@gmail.com>
2021-03-02 12:01:15 +0000

    Fix: Height of settings now fills whole screen

--- src/html/pages/settings-security.php
+++ src/html/pages/settings-security.php
@@ -16,8 +16,8 @@ $stylesheets = array(
 
         <?php site_header("Foo Fighters", "page_settings"); ?>
 
-        <div class="container-fluid" style="flex:auto;">
-            <div class="row">
+        <div class="container-fluid" style="flex:auto; height: 0">
+            <div class="row h-100">
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
                     <div class="position-sticky pt-3">
                     
