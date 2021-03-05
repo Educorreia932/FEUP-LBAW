3b083e607f620aad544690606c6189436fb309da
Tiago Duarte da Silva <up201806516@fe.up.pt>
2021-03-05 15:58:23 +0000

    Added breadcrumbs Added couple new styles to fix minor details in carousels Removed useless js file

--- src/html/js/tooltip_initializer.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
