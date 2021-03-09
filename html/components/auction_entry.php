e04f33c6774357b5b10edb9d07a5bd6e1cab6e4a
Tiago Duarte da Silva <up201806516@fe.up.pt>
2021-03-09 09:30:28 +0000

    Added admin dashboard Removed height 'fix' from settings

--- html/components/auction_entry.php
+++ html/components/auction_entry.php
@@ -62,5 +62,15 @@ function filter_checkbox($name, $id, $checked = false, $master = false) { ?>
 <?php 
 }
 
+function filter_radio($label, $name, $id, $checked = false, $disabled = false) { ?>
 
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="<?= $name ?>" id="<?= $id ?>" <?= $checked ? "checked" : "" ?> <?= $disabled ? "disabled" : "" ?>>
+        <label class="form-check-label" for="<?= $id ?>">
+            <?= $label ?>
+        </label>
+    </div>
+
+<?php 
+}
 ?>
