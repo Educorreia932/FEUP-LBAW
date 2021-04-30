a34747f5ddb1a80b5f1ef0c6e644450df3244f4f
ivSaav <ivo_saavedra@outlook.com>
2021-04-30 14:42:31 +0100

    Fix: restored docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo docker run -it -p 8080:80 -v $PWD/html:/var/www/html lbaw2112/lbaw2112-piu
+set -e
+
+cd /var/www; php artisan config:cache
+env >> /var/www/.env
+php-fpm7.4 -D
+nginx -g "daemon off;"
