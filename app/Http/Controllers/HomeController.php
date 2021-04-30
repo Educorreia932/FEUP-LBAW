7929d413c2c10f232b08d721ff7bb48c13da16d6
Educorreia932 <skelozard1@gmail.com>
2021-05-01 00:38:09 +0100

    Verifying if the user is logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Auction;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller {
     public function show() {
+        $user = Auth::user();
+
         $open_auctions = Auction::all()->take(6);
 
-        return view('pages.home', ["open_auctions" => $open_auctions]);
+        return view('pages.home', ["open_auctions" => $open_auctions, "user" => $user]);
     }
 }
