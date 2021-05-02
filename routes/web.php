2d352944589a67042935950f8e3427bfc7c293c0
ivSaav <ivo_saavedra@outlook.com>
2021-05-02 23:33:44 +0100

    Add: auction edit and report in openapi documentation

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('user/search_results', 'SearchResultsController@search_users')->name(
 // Auctions
 Route::get("auction/{id}", "AuctionController@show")->where('id', '[0-9]+')->name("auction");
 Route::get("auction/{id}/details", "AuctionController@showDetails")->where('id', '[0-9]+')->name("auction_details");
-Route::get("create_auction", "AuctionController@create")->name("create_auction")->middleware("auth");
-Route::post("create_auction", "AuctionController@store")->name("store_auction")->middleware("auth");
+Route::get("auction/create", "AuctionController@create")->name("create_auction")->middleware("auth");
+Route::post("auction/create", "AuctionController@store")->name("store_auction")->middleware("auth");
 
 
 // Users
