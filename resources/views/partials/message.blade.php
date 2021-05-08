6d109ff9c0d4408c3136349689aa4f7db6eb2ee5
Educorreia932 <skelozard1@gmail.com>
2021-05-08 16:34:37 +0100

    Setting max width for messages

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,19 +1,21 @@
-@if( $message->sender == Auth::user() )
-    <div class="message rounded p-2 bg-primary text-white">
-        <a class="link-light text-decoration-none" href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
-            <strong>{{ $message->sender->name }}</strong>
-        </a>
-        <p class="m-0">{{ $message->body }}</p>
-    </div>
-@else
-    <div class="message rounded p-2 bg-white">
-        <a class="link-dark text-decoration-none" href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
-            <strong>{{ $message->sender->name }}</strong>
-        </a>
-        <p class="m-0">{{ $message->body }}</p>
-    </div>
-@endif
+<div class="message d-block">
+    @if( $message->sender == Auth::user() )
+        <div class="bg-primary text-white mw-50 d-inline-block p-2 rounded">
+            <a class="link-light text-decoration-none"
+               href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
+                <strong>{{ $message->sender->name }}</strong>
+            </a>
+            <p class="m-0">{{ $message->body }}</p>
+        </div>
 
-
-
+    @else
+        <div class="bg-white mw-50 d-inline-block p-2 rounded">
+            <a class="link-dark text-decoration-none"
+               href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
+                <strong>{{ $message->sender->name }}</strong>
+            </a>
+            <p class="m-0">{{ $message->body }}</p>
+        </div>
+    @endif
+</div>
 
