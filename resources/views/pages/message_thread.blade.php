f16afca932745941c71f6459eb3ac1ee2e8f270b
Educorreia932 <skelozard1@gmail.com>
2021-05-08 17:51:47 +0100

    Created an input form to send messages

--- resources/views/pages/message_thread.blade.php
+++ resources/views/pages/message_thread.blade.php
@@ -16,5 +16,17 @@
                 @include("partials.message")
             @endforeach
         </section>
+
+        {{-- Send message --}}
+        <form class="container input-group my-4">
+            <input type="text" class="form-control"
+                   placeholder="Write a message"
+                   aria-label="Write a message"
+                   aria-describedby="send-addon"
+            />
+            <button id="send-addon" class="input-group-text border-0">
+                <i class="bi bi-arrow-up-right"></i>
+            </button>
+        </form>
     </section>
 @endsection
