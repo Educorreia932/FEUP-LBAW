ce6a54733ac74450422c740af49e6445bdb483c0
Educorreia932 <skelozard1@gmail.com>
2021-05-08 16:19:57 +0100

    Added hyperlinks to the message's senders

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,11 +1,15 @@
 @if( $message->sender == Auth::user() )
     <div class="message rounded p-2 bg-primary text-white">
-        <strong>{{ $message->sender->username }}</strong>
+        <a class="link-light text-decoration-none" href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
+            <strong>{{ $message->sender->name }}</strong>
+        </a>
         <p class="m-0">{{ $message->body }}</p>
     </div>
 @else
     <div class="message rounded p-2 bg-white">
-        <strong>{{ $message->sender->username }}</strong>
+        <a class="link-dark text-decoration-none" href="{{ route("user_profile", [ "username" => $message->sender->username ]) }}">
+            <strong>{{ $message->sender->name }}</strong>
+        </a>
         <p class="m-0">{{ $message->body }}</p>
     </div>
 @endif
