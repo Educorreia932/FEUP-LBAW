58042c56535945e74d2c670987bb08da7e1fcbb0
ivSaav <ivo_saavedra@outlook.com>
2021-05-20 14:19:31 +0100

    Add: missing file

---
+++ resources/views/pages/admin/reported_users.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.dashboard', ['sub' => 'reported_users'])
+
+@section('subpage')
+
+    <div class="container-fluid mb-4">
+        <div class="my-4">
+            <h2>Reported Users</h2>
+
+            @include("partials.breadcrumbs", [ "pages" => [
+                ["title" => "Home", "href" => "/"],
+                ["title" => "Me", "href" => "/users/me"],
+                ["title" => "Dashboard", "href" => "/dashboard"]
+            ]])
+        </div>
+
+        <div class="table-responsive col-12">
+            <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Reported for</th>
+                <th scope="col">Details</th>
+                <th scope="col">Date</th>
+                {{-- <th scope="col">Action</th> --}}
+                </tr>
+            </thead>
+            <tbody>
+                {{-- Reported user entries --}}
+                @foreach ($reports as $report)
+                    <tr class="user-entry align-middle" user_id="{{$report->member_id}}">
+                        <th scope="row">{{$report->username}}</th>
+                        <td>
+                            {{$report->reason}}
+                        </td>
+                        <td>
+                            {{ $report->description}}
+                        </td>
+                        <td>{{$report->timestamp}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+            </table>
+        </div>  
+
+
+        <nav class="d-flex justify-content-center my-4">
+            {!! $reports->appends(request()->except('page'))->links() !!}
+        </nav>
+    </div>
+@endsection
