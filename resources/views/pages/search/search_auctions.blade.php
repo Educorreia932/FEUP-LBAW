528318620b55a03c9fce692933be8bdefc1ddae8
ivSaav <ivo_saavedra@outlook.com>
2021-06-04 02:34:53 +0100

    Fix: semantical tags for search_auctions and aria labels

--- resources/views/pages/search/search_auctions.blade.php
+++ resources/views/pages/search/search_auctions.blade.php
@@ -21,7 +21,7 @@
 @endsection
 
 @section("results")
-    <p>Results {{$auctions->firstItem()}}-{{$auctions->lastItem()}} for: <u>{{ old('fts', 'All') }}</u> ({{ $auctions->total() }})</p>
+    <span>Results {{$auctions->firstItem()}}-{{$auctions->lastItem()}} for: <u>{{ old('fts', 'All') }}</u> ({{ $auctions->total() }})</span>
 
     @if (count($auctions))
         {{-- display auctions --}}
@@ -59,11 +59,8 @@
     <script defer src={{ asset("js/init_tooltips.js") }}></script>
 
     {{-- Categories --}}
-    <section>
-        <p class="text-secondary my-2">Category</p>
-
-    <div class="master-checkbox-reverse">
-        {{-- @include('partials.filter_checkbox', ["name" => "All", "id" => "all", "checked" => old('filter_check_all', true), "master" => true]) --}}
+    <section role="group" aria-labelledby="ctg_title">
+        <h6 id="ctg_title" class="text-secondary my-2">Category</h6>
 
         <div class="row">
             <div class="col">
@@ -82,13 +79,12 @@
                 @endforeach
             </div>
         </div>
-        </div>
     </section>
 
     @can('viewNsfw', App\Models\Auction::class)
     {{-- Nsfw --}}
-    <section class="my-3" id="auction-safety">
-        <p class="text-secondary my-2">Age Rating</p>
+    <section class="my-3" id="auction-safety" role="group" aria-labelledby="age_filter_title">
+        <h6 id="age_filter_title" class="text-secondary my-2">Age Rating</h6>
 
         @include('partials.filter_checkbox', ["name" => "Everyone", "group" => "nsfw", "value" => "sfw", "request" => $request, "default" => true])
         @include('partials.filter_checkbox', ["name" => "NSFW", "group" => "nsfw", "value" => "nsfw", "request" => $request])
@@ -96,8 +92,8 @@
     @endcan
 
     {{-- Auction Owner --}}
-    <section class="my-3" id="auction-owner-radios">
-        <p class="text-secondary my-2">Auction Owner</p>
+    <section class="my-3" id="auction-owner-radios" role="group" aria-labelledby="auction_filter_title">
+        <h6 id="auction_filter_title" class="text-secondary my-2">Auction Owner</h6>
 
         <div class="form-check">
             <input class="form-check-input" type="radio" name="owner_filter" id="radio-owner-any" value="any"
@@ -123,13 +119,13 @@
         <div class="input-group mb-3">
             <span class="input-group-text" id="radio-owner-user-at">@</span>
             <input type="text" id="radio-owner-user-input" class="form-control" placeholder="Username"
-                    aria-label="Username" aria-describedby="radio-owner-user-at" name="fts_user" value="{{ old('fts_user') }}" {{ old('fts_user') ? "" : "disabled" }}>
+                    aria-label="username" role="searchbox" aria-describedby="radio-owner-user-at" name="fts_user" value="{{ old('fts_user') }}" {{ old('fts_user') ? "" : "disabled" }}>
         </div>
     </section>
 
     {{-- Auction timeframe --}}
-    <section class="my-3">
-        <p class="text-secondary my-2">Auction timeframe</p>
+    <section class="my-3" role="group" aria-labelledby="auction_time_title">
+        <h6 id="auction_time_title" class="text-secondary my-2">Auction timeframe</h6>
 
         @include('partials.filter_checkbox', ["name" => "Scheduled", "group" => "timeframe", "value" => "sched", "request" => $request, "default" => true])
         @include('partials.filter_checkbox', ["name" => "Open", "group" => "timeframe", "value" => "open", "request" => $request, "default" => true])
@@ -137,29 +133,27 @@
     </section>
 
     {{-- Current bid price range --}}
-    <div class="my-3">
-        <label class="text-secondary" for="price-range">Current bid (Φ)</label>
+    <section class="my-3" aria-labelledby="current_bid_title">
+        <h6 id="current_bid_title" class="text-secondary">Current bid (Φ)</h6>
 
-        <div class="row">
-            <div class="d-flex d-none d-md-flex">
-                <div id="price-range-slider" class="my-2 mx-4 w-100"></div>
-            </div>
+        <div class="d-flex d-none d-md-flex">
+            <div id="price-range-slider" role="slider" class="my-2 mx-4 w-100"></div>
         </div>
 
         <div class="row mb-3">
             <div class="col-sm col-md-12 col-lg d-flex flex-column align-items-stretch">
                 <label for="input-number-left" class="form-label text-secondary mb-0">Min</label>
                 <input type="number" min="0" class="form-control hide-appearence" id="input-number-left"
-                        aria-label="Amount" name="min_bid" value="{{ old('min_bid', 0) }}">
+                        aria-label="bid amount" name="min_bid" value="{{ old('min_bid', 0) }}">
             </div>
 
             <div class="col-sm col-md-12 col-lg d-flex flex-column align-items-stretch">
                 <label for="input-number-right" class="form-label text-secondary mb-0">Max</label>
                 <input type="number" min="0" class="form-control hide-appearence" id="input-number-right"
-                        aria-label="Amount" name="max_bid" value="{{ old('max_bid', 500) }}">
+                        aria-label="bid amount" name="max_bid" value="{{ old('max_bid', 500) }}">
             </div>
         </div>
-    </div>
+    </section>
 @endsection
 
 @section("links")
